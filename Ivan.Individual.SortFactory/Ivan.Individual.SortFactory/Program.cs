2ee684751ebbb9a44a85604cffdcb52de077579d
niguanglun <niguanglun@users.noreply.github.com>
2020-06-05 09:54:54 +0800

    v1.5

--- Ivan.Individual.SortFactory/Ivan.Individual.SortFactory/Program.cs
+++ Ivan.Individual.SortFactory/Ivan.Individual.SortFactory/Program.cs
@@ -12,21 +12,21 @@ namespace Ivan.Individual.SortFactory
         static void Main(string[] args)
         {
             //Example for c# Built-in types
-//            ISortFactory<int> m_sortFactory = new HeapSortFactory<int>();
-//            int[] array = { 6, 9, 7, 5, 4, 2, 1, 8, 0 };
-//            m_sortFactory.CreateSort().Sort(array, AscComparison);
+            ISortFactory<int> m_sortFactory = new HeapSortFactory<int>();
+            int[] array = { 6, 9, 7, 5, 4, 2, 1, 8, 0 };
+            m_sortFactory.CreateSort().Sort(array, AscComparison);
 
             //Example for Person
-//              ISortFactory<Person> m_sortPersonFactory = new BubbleSortFactory<Person>();
-//              Person[] personArray = { new Person("aa", 1), new Person("cc", 3), new Person("dd", 4), new Person("bb", 2) };
-//              var bubbleSort = m_sortPersonFactory.CreateSort();
-//             bubbleSort.Sort(personArray);
+            ISortFactory<Person> m_sortPersonFactory = new BubbleSortFactory<Person>();
+            Person[] personArray = { new Person("aa", 1), new Person("cc", 3), new Person("dd", 4), new Person("bb", 2) };
+            var bubbleSort = m_sortPersonFactory.CreateSort();
+            bubbleSort.Sort(personArray);
 
             //Example for Point
             ISortFactory<Point> m_sortPointFactory = new QuickSortFactory<Point>();
             Point[] pointArray = { new Point(3, 1), new Point(1, 3), new Point(5, 4), new Point(0, 9) };
             m_sortPointFactory.CreateSort().Sort(pointArray);
-             
+
             Console.ReadKey();
 
         }
@@ -45,8 +45,5 @@ namespace Ivan.Individual.SortFactory
                 return 1;
             }
         }
-  
-
     }
 }
- 
